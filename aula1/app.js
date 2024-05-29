8c1b266504dac6866ee0c938b62ac4281c8429bc
mateuslll <lima.mt96@gmail.com>
2024-05-29 07:16:51 -0300

    vue course class-9

--- aula1/app.js
+++ aula1/app.js
@@ -1,10 +1,26 @@
+Vue.createApp({
+    data (){
+        return{
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal () {
+            this.goals.push(this.enteredValue)
+            this.enteredValue = ''
+        }
+    }
+
+}).mount('#app');
+
 //representaçao do button esta armazenado no buttonEl 
 const buttonEl = document.querySelector('button'); 
-const inputEl = document.querySelector('goal'); 
+const inputEl = document.querySelector('input'); 
 const listEl = document.querySelector('ul'); 
 
 function addGoal(){ 
-    const enteredValue = input.valueEl; 
+    const enteredValue = inputEl.value; 
     const listItem = document.createElement('li'); 
     listItem.textContent = enteredValue; 
     listEl.appendChild(listItem); 
